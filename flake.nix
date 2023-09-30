4ce3b4e2f029135a3db52b5a802565bcf8caf9b3
Krusl Lee <peigong2013@outlook.com>
2023-10-01 03:38:42 +0800

    fractal-next

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  outputs = { self, nixpkgs, ... }: let 
+    pkgs = import nixpkgs { localSystem = { system = "x86_64-linux"; }; }
+  in {
+    fractal-next = pkgs.fractal-next;
+  };
+}
